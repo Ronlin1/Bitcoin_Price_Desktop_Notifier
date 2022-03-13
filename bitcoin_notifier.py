9b6a11374d71a972a168e7b766f7f91d51196b74
Ronnie Atuhaire <66623378+Ronlin1@users.noreply.github.com>
2022-03-13 17:30:42 +0300

    push all

---
+++ bitcoin_notifier.py
@@ -0,0 +1,46 @@
+import requests
+import time
+from plyer import notification
+import datetime
+
+alert_amount = 50000
+bitcoin_rate = None
+
+now = datetime.datetime.now()
+now = now.strftime("%d/%m %H:%M")
+
+URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+try:
+    response = requests.get(URL,
+      headers={"Accept": "application/json"},
+    )
+    data = response.json()
+    bpi = data['bpi']
+    USD = bpi['USD']
+    bitcoin_rate = int(USD['rate_float'])
+
+    print("[+] We are live [+]")
+
+except:
+    print('Something is wrong, Do you have Internet!')
+
+
+while bitcoin_rate:
+        notification.notify(
+            #title of the notification,
+            title = f"Bitcoin Price Alert!! {now}",
+
+            #the body of the notification
+            message = f"Current Bitcoin Price is {bitcoin_rate}",
+
+            #creating icon for the notification
+            #we need to download a icon of ico file format
+            app_icon = "bitcoin.ico",
+
+            # the notification stays for 60 seconds
+            timeout  = 60
+        )
+
+        #notification repeats after every 5 Minutes
+        time.sleep(300)
